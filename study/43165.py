a7eab2737fa11daa70cd0706a1c957813f0c9208
sujin-park0607 <goweg0607@gmail.com>
2022-12-11 21:41:30 +0900

    refactor: programmers 타겟넘버 수정

--- study/43165.py
+++ study/43165.py
@@ -1,20 +1,36 @@
 # numbers를 사용해서 -,+를 해서 target숫자로 만들기
 # 1이상 1000이하일때 가능
-from collections import deque
 
-numbers = [1, 1, 1, 1, 1]
-visited = [False] * len(numbers)
-target = 3
-print(visited)
 
-def bfs(index):
-    visited[index] = True
-    current = numbers[index]
-    
-    for cal in "-","+":
-        index += 1
-        after = eval(str(current)+cal+str(numbers[index]))
-        print(after)
-        if after <= 0 or after >= 1000:
+def solution(numbers, target):
+    #index를 넘어가는 로직이기때문에 마지막에 0 추가
+    numbers.append(0)
+
+    #초기값 설정
+    stack = [(0,0)]
+    answer = 0
+
+    #dfs
+    while stack:
+        index, result = stack.pop()
+        #만약 주어진 배열의 index를 넘어갈때 결과값을 보고 answer에 추가 
+        if index == (len(numbers))-1:
+            if result == target:
+                answer += 1
             continue
-        if not visited[index]
+        #index가 넘어가지 않을 경우 그대로 반복
+        else:
+            stack.append((index+1, result - numbers[index]))
+            stack.append((index+1, result + numbers[index]))
+    return answer
+
+
+numbers = [4, 1, 2, 1]
+target = 4
+
+# numbers = [1, 1, 1, 1, 1]
+# target = 3
+print(solution(numbers, target))
+
+
+    
