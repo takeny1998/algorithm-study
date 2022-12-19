c8437457937dbd8ad406fefb722de40a6cb33f9a
sujin-park0607 <goweg0607@gmail.com>
2022-12-20 02:31:01 +0900

    feat: backjoon 12100

--- sujin/study/12100.py
+++ sujin/study/12100.py
@@ -1,60 +1,176 @@
-from collections import deque
-n = int(input())
-board = [list(map(int, input().split())) for _ in range(n)]
-answer, q = 0, deque()
+# 구현문제가 중시
+# 최대 5번 이동해서 만들 수 있는 가장 큰 블록
+# 상,하,좌,우 * 다섯번
+# 순서 중요하니깐 순열
+# 4P5
+from itertools import product
 
-def get(i, j):
-    if board[i][j]: # 0이 아닌 값이라면
-        q.append(board[i][j]) # queue에 board의 값을 넣는다.
-        board[i][j] = 0 # 처리가 된 빈 자리는 0으로 값 업데이트
-        
-def merge(i, j, di, dj): # row index, column index, y방향, x방향 
-    while q:
-        x = q.popleft() # 움직이려는 블록 값을 가져온다. FIFO 
-        if not board[i][j]: # 0이라면 그대로 놓는다.
-            board[i][j] = x
-        elif board[i][j] == x: # 값이 일치한다면
-            board[i][j] = x*2 # 합쳐지므로 2배로 증가
-            i, j = i+di, j+dj 
-        else: # 값이 일치하지 않으면
-            i, j = i+di, j+dj
-            board[i][j] = x 
-
-def move(k):
-    # board[i][j]
-    if k == 0: # 위로 이동, 블락들이 위로 모두 이동하면 row index는 0
-        for j in range(n):
-            for i in range(n):
-                get(i, j)
-            merge(0, j, 1, 0) # row index 1씩 증가하면서 아래쪽 블락들을 합쳐감
-    elif k == 1: # 아래로 이동, 블락들이 아래로 모두 이동하면 row index는 n-1
-        for j in range(n):
-            for i in range(n-1, -1, -1):
-                get(i, j)
-            merge(n-1, j, -1, 0) # row 인덱스 1씩 감소하면서 위쪽들을 합쳐감
-    elif k == 2: # 오른쪽으로 이동, column index는 0
-        for i in range(n):
-            for j in range(n):
-                get(i, j)
-            merge(i, 0, 0, 1) # column 인덱스 증가 오른쪽으로 이동
-    else: # 왼쪽으로 이동, column index는 n-1
-        for i in range(n):
-            for j in range(n-1, -1, -1):
-                get(i, j)
-            merge(i, n-1, 0, -1) # column 인덱스 감소 왼쪽으로 이동
-            
-def solve(count):
-    global board, answer
-    if count == 5: # 최대 5번까지 움직였다면
-        for i in range(n):
-            answer = max(answer, max(board[i])) # 가장 큰 값이 answer
-        return
-    b = [x[:] for x in board] # 방향을 바꾸기 전에 원래의 보드를 기억해야 한다.
+#빈곳이 없도록 한쪽으로 미는 함수
+def move(board, direction):
+    #우
+    if direction == (1,0):
+        for i in range(size):
+            arr = list()
+            cnt = 0
+            for j in range(size):
+                if board[i][j] == 0:
+                    cnt += 1
+                else:
+                    arr.append(board[i][j])
+            board[i] = [0] * cnt + arr
+    #좌
+    elif direction == (-1,0):
+        for i in range(size):
+            arr = list()
+            cnt = 0
+            for j in range(size):
+                if board[i][j] == 0:
+                    cnt += 1
+                else:
+                    arr.append(board[i][j])
+            board[i] = arr + [0] * cnt
+    #상
+    elif direction == (0, 1):
+        new_board = [[0] * size for i in range(size)]
+        for i in range(size):
+            arr = list()
+            cnt = 0
+            for j in range(size):
+                if board[j][i] == 0:
+                    cnt += 1
+                else:
+                    arr.append(board[j][i])
+            new_board[i] = arr + [0] * cnt
+        board = list(map(list,zip(*new_board)))
     
-    for k in range(4): # 4방향으로 움직인다.
-        move(k) # 움직인다.
-        solve(count+1) # 재귀적으로 호출한다.
-        board = [x[:] for x in b]
+     #하
+    elif direction == (0, -1):
+        new_board = [[0] * size for i in range(size)]
+        for i in range(size):
+            arr = list()
+            cnt = 0
+            for j in range(size):
+                if board[j][i] == 0:
+                    cnt += 1
+                else:
+                    arr.append(board[j][i])
+            new_board[i] = [0] * cnt + arr
+        board = list(map(list,zip(*new_board)))
+    return board
 
-solve(0)
-print(answer)
+
+#같은쪽 계산함수
+def calculation(board, direction, maxNum):
+    #우
+    if direction == (1,0):
+        stamp = [[False] * size for i in range(size)]
+        for i in range(size):
+            for j in range(size-2, -1, -1):
+                if board[i][j] == board[i][j+1] and not stamp[i][j+1]:
+
+                    stamp[i][j] = True
+                    stamp[i-1][j] = True
+
+                    board[i][j+1] += board[i][j]
+                    board[i][j] = 0
+
+                    if board[i][j+1] > maxNum:
+                        maxNum = board[i][j+1]
+                else:
+                    continue
+    
+    #좌
+    elif direction == (-1,0):
+        stamp = [[False] * size for i in range(size)]
+        for i in range(size):
+            for j in range(1, size-1, 1):
+                if board[i][j] == board[i][j-1] and not stamp[i][j-1]:
+
+                    stamp[i][j] = True
+                    stamp[i][j-1] = True
+
+                    board[i][j-1] += board[i][j]
+                    board[i][j] = 0
+
+                    if board[i][j-1] > maxNum:
+                        maxNum = board[i][j-1]
+                else:
+                    continue
+    
+
+    #상
+    elif direction == (0,1):
+        stamp = [[False] * size for i in range(size)]
+        for i in range(1, size-1, +1):
+            for j in range(size):
+                if board[i][j] == board[i-1][j]:
+                    
+                    stamp[i][j] = True
+                    stamp[i-1][j] = True
+
+                    board[i-1][j] += board[i][j]
+                    board[i][j] = 0
+
+                    if board[i-1][j] > maxNum:
+                        maxNum = board[i-1][j]
+            
+                else:
+                    continue
+    
+    #하
+    elif direction == (0,-1):
+        stamp = [[False] * size for i in range(size)]
+        for i in range(size-2, -1, -1):
+            for j in range(size):
+                if board[i][j] == board[i+1][j]:
+                    
+                    stamp[i][j] = True
+                    stamp[i+1][j] = True
+
+                    board[i+1][j] += board[i][j]
+                    board[i][j] = 0
+
+                    if board[i+1][j] > maxNum:
+                        maxNum = board[i-1][j]
+                else:
+                    continue
+    
+    return board, maxNum
+    
+# 한번 방향키마다 작동하는 함수         
+def turn(board, direction, maxNum):
+    board = move(board, direction)
+    board, maxNum = calculation(board,direction, maxNum)
+    board = move(board, direction)
+    return board, maxNum
+
+    
+            
+
+# 상하좌우로 5번 가는 가는 모든 경우의 수 도출
+direction = [(1,0), (-1,0), (0,1), (0,-1)]
+case = list(product(direction,direction,direction,direction,direction))
+
+#게임판 만들기 
+size = int(input())
+board = list()
+for i in range(size):
+    board.append(list(map(int, input().split(" "))))
+
+maxNum = 0
+for directions in case:
+    test_board = board
+
+    for direction in directions:
+        board, maxNum = turn(board, direction, maxNum)
+
+print(maxNum)
+
+                
+        
+    
+
+
+
+
+    
